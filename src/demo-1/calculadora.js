256f7ddc23f32f90857ebbf4f185398525bf1090
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-30 23:54:51 -0300

    Desenvolvimento de um teste simples com mocha & chai.

--- src/demo-1/calculadora.js
+++ src/demo-1/calculadora.js
@@ -9,23 +9,15 @@
 
 module.exports = {
   // Função responsável por adicionar 2 números:
-  adicionar: (num1, num2) => {
-    return num1 + num2;
-  },
+  adicionar: (num1, num2) => num1 + num2,
 
   // Função responsável por subtrair 2 números:
-  subtrair: (num1, num2) => {
-    return num1 - num2;
-  },
+  subtrair: (num1, num2) => num1 - num2,
 
   // Função responsável por multiplicar 2 números:
-  multiplicar: (num1, num2) => {
-    return num1 * num2;
-  },
+  multiplicar: (num1, num2) => num1 * num2,
 
   // Função responsável por dividir 2 números:
-  dividir: (num1, num2) => {
-    return num1 / num2;
-  },
+  dividir: (num1, num2) => num1 / num2,
 };
 
---
+++ src/test/calculadora-test.js
@@ -0,0 +1,34 @@
+/**
+ * Arquivo: calculadora-test.js
+ * Author: Glaucia Lemos
+ * Description: arquivo responsável por realizar os testes do arquivo: 'calculadora.js'
+ * Data: 30/03/2018
+ *
+ * Documentação: http://chaijs.com/guide/styles/#assert
+ *
+ */
+
+// Aqui estou usando a sintaxe: Assert (chai):
+const chai = require('chai');
+const calculadora = require('../demo-1/calculadora');
+
+const assert = chai.assert;
+
+// Primeiro Teste:
+describe('TDD de Operações da Calculadora', () => {
+  it('Teste: Deve Somar 2 Números', () => {
+    assert.equal(calculadora.adicionar(5, 5), 10);
+  });
+
+  it('Teste: Deve Subtrair 2 Números', () => {
+    assert.equal(calculadora.subtrair(10, 5), 5);
+  });
+
+  it('Teste: Deve Multiplicar 2 Números', () => {
+    assert.equal(calculadora.multiplicar(10, 5), 50);
+  });
+
+  it('Teste: Deve dividir 2 Números', () => {
+    assert.equal(calculadora.dividir(18, 2), 9);
+  });
+});
