b32f06ec7fddcf2231f723123c1b4bb2cb052b66
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-30 22:39:28 -0300

    Desenvolvimento do arquivo que realiza as 4 operações da calculadora.

---
+++ src/demo-1/calculadora.js
@@ -0,0 +1,31 @@
+/**
+ *
+ * Arquivo: calculadora.js
+ * Author: Glaucia Lemos
+ * Description: arquivo responsável por executar as 4 operações de uma calculadora: + - * /
+ * Data: 30/03/2018
+ *
+ */
+
+module.exports = {
+  // Função responsável por adicionar 2 números:
+  adicionar: (num1, num2) => {
+    return num1 + num2;
+  },
+
+  // Função responsável por subtrair 2 números:
+  subtrair: (num1, num2) => {
+    return num1 - num2;
+  },
+
+  // Função responsável por multiplicar 2 números:
+  multiplicar: (num1, num2) => {
+    return num1 * num2;
+  },
+
+  // Função responsável por dividir 2 números:
+  dividir: (num1, num2) => {
+    return num1 / num2;
+  },
+};
+
